151e1dd7a4dffb16bbd09e692782bd5f8851c49c
Erika de Almeida Ramos <erikaramosalmeida90@gmail.com>
2021-04-24 00:34:08 -0300

    implementacaoAgentesInteligentes

--- src/br/fatec/smartbooking/model/Dialogue.java
+++ src/br/fatec/smartbooking/model/Dialogue.java
@@ -1,43 +1,70 @@
 package br.fatec.smartbooking.model;
 
-public class Dialogue {
+import java.util.ArrayList;
+
+import jade.util.leap.Serializable;
+
+public class Dialogue implements Serializable {
 	private String question;
 	private String answer;
 	private int behaviour;
-	
-	public Dialogue(String question, String answer, int behaviour) {
+	private ArrayList<Room> roomsAvailable;
+	private Booking customerBooking;
+
+	public Dialogue(String answer) {
 		super();
-		this.question = question;
 		this.answer = answer;
-		this.behaviour = behaviour;
+		this.behaviour = 1;
 	}
-	
+
 	public Dialogue() {
 		super();
+		this.behaviour = 1;
 	}
 
 	public String getQuestion() {
 		return question;
 	}
+
 	public void setQuestion(String question) {
 		this.question = question;
 	}
+
 	public String getAnswer() {
 		return answer;
 	}
+
 	public void setAnswer(String answer) {
 		this.answer = answer;
 	}
+
 	public int getBehaviour() {
 		return behaviour;
 	}
+
 	public void setBehaviour(int behaviour) {
 		this.behaviour = behaviour;
+	}
+
+	public ArrayList<Room> getRoomsAvailable() {
+		return roomsAvailable;
+	}
+
+	public void setRoomsAvailable(ArrayList<Room> roomsAvailable) {
+		this.roomsAvailable = roomsAvailable;
+	}
+
+	public Booking getCustomerBooking() {
+		return customerBooking;
+	}
+
+	public void setCustomerBooking(Booking customerBooking) {
+		this.customerBooking = customerBooking;
 	}
 
 	@Override
 	public String toString() {
 		return this.getAnswer();
 	}
-	
+
 }
